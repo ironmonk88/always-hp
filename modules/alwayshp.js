6c98361abc242bcd9bbbc6e903de9a100f98343e
ironmonk88 <75920956+ironmonk88@users.noreply.github.com>
2020-12-18 16:08:06 -0800

    Add files via upload

---
+++ modules/alwayshp.js
@@ -0,0 +1,24 @@
+import { registerSettings } from "./modules/registerSettings.mjs";
+
+export let debug = (...args) => {
+    if (debugEnabled > 1) console.log("DEBUG: alwayshp | ", ...args);
+};
+export let log = (...args) => console.log("alwayshp | ", ...args);
+export let warn = (...args) => {
+    if (debugEnabled > 0) console.warn("alwayshp | ", ...args);
+};
+export let error = (...args) => console.error("alwayshp | ", ...args);
+export let i18n = key => {
+    return game.i18n.localize(key);
+};
+
+export class AlwaysHP extends Application {
+
+    static get defaultOptions() {
+        const options = super.defaultOptions;
+        options.template = "modules/always-hp/templates/alwayshp.html";
+        options.popOut = false;
+        options.resizable = false;
+        return options;
+    }
+}
